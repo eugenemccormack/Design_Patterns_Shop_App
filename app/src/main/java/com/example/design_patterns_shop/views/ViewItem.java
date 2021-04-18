a4e288e78622cdce4bb79b884d8cac8a00e0f859
Eugene McCormack <c17745919@mytudublin.ie>
2021-04-18 23:42:24 +0100

    Picture Upload Added & View Cart Items / Remove from Cart - Firebase Remove

--- app/src/main/java/com/example/design_patterns_shop/views/ViewItem.java
+++ app/src/main/java/com/example/design_patterns_shop/views/ViewItem.java
@@ -1,10 +1,17 @@
 package com.example.design_patterns_shop.views;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 import com.example.design_patterns_shop.Adapter.MyAdapter;
 import com.example.design_patterns_shop.R;
 import com.example.design_patterns_shop.model.CartModel;
@@ -18,6 +25,7 @@ public class ViewItem extends AppCompatActivity {
     public DatabaseReference db;
     private FirebaseAuth firebaseAuth;
 
+    Uri myUri;
 
     Button addToCart;
 
@@ -27,6 +35,14 @@ public class ViewItem extends AppCompatActivity {
         setContentView(R.layout.view_item);
 
         Intent intent = getIntent();
+
+        final String image = intent.getStringExtra(MyAdapter.MESSAGE_KEY);
+        ImageView text1 = (ImageView) findViewById(R.id.imageView2);
+
+        myUri = Uri.parse(image);
+
+        Glide.with(ViewItem.this).load(myUri).into(text1);
+
         final String title = intent.getStringExtra(MyAdapter.MESSAGE_KEY1);
         TextView text2 = (TextView) findViewById(R.id.textView_View);
         text2.setText(title);
@@ -55,21 +71,17 @@ public class ViewItem extends AppCompatActivity {
 
             db = FirebaseDatabase.getInstance().getReference();
 
-            CartModel cart = new CartModel(title, manufacturer, category, price);
+            CartModel cart = new CartModel(image, title, manufacturer, category, price);
 
             db.child("Users").child(UserID).child("Cart").child(title).setValue(cart);
+
+            Toast.makeText(ViewItem.this, "Item Added to your Cart", Toast.LENGTH_SHORT).show();
 
 
         });
 
 
 
-
-/*        final double price = Double.parseDouble(intent.getDoubleExtra(MyAdapter.MESSAGE_KEY4));
-        TextView text5 = (TextView) findViewById(R.id.price_textView);
-        text5.setText("€" + price);*/
-
-        //Log.w("DBError", "Price: " + price);
 
 /*        final String stock = intent.getStringExtra(MyAdapter.MESSAGE_KEY5);
         TextView text6 = (TextView) findViewById(R.id.stock_textView);
